9bfa1fd952c7fdbf972586d0ba6626ed57da31b1
goosun kim <misschivass85@gmail.com>
2025-12-21 10:53:17 +0900

    Fix Naver verification code format

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,11 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Pawcast - 반려견 산책 필수 앱", // 타이틀에 키워드 하나 추가 추천
   description: "반려견을 아끼는 진심 반려인의 필수 앱 포캐스트. 강아지 산책 환경 등급과 맞춤형 날씨 정보를 제공하는 반려견 산책 어플리케이션입니다.", // 어플리케이션 키워드 추가
+  verification: {
+    other: {
+      "naver-site-verification": "fdb2c15410e5122fd9793afe15929d0e94b9181b",
+    },
+  },
   icons: {
     icon: '/icon.png',
   },
